6dbae0bd2e37d1682af14be123f20b87d89ea9f9
devatherock <devatherock@gmail.com>
2023-12-02 08:41:02 -0600

    SO-44950361: Used a docker agent

--- jobs/pipelines/test.groovy
+++ jobs/pipelines/test.groovy
@@ -1,43 +1,10 @@
-import groovy.xml.XmlUtil
-import groovy.util.XmlSlurper
-
-@NonCPS
-void updateFile() {
-    String xmlText = '''
-    <configuration>
-        <appender name="STDOUT" class="ch.qos.logback.core.ConsoleAppender">
-            <withJansi>true</withJansi>
-            <encoder>
-                <pattern>%d{HH:mm:ss.SSS} [%thread] %-5level %logger{10} - %msg%n</pattern>
-            </encoder>
-        </appender>
-    
-        <root level="INFO">
-            <appender-ref ref="STDOUT" />
-        </root>
-    </configuration>
-    '''.stripIndent().trim()
-    def xml = new XmlSlurper().parseText(xmlText)
-
-    def modifiedXml = XmlUtil.serialize(xml)
-    writeFile(file: 'logback.xml', text: modifiedXml)
-}
-
 pipeline {
-    agent any
+    agent { docker 'alpine' }
 
     stages {
-        stage('parse xml') {
+        stage('Test') {
             steps {
-                script {
-                    updateFile()
-                }
-            }
-        }
-
-        stage('read xml') {
-            steps {
-                sh 'cat logback.xml'
+                sh('printenv')
             }
         }
     }
