2a3cdb89db1120d5eaea7f3765fa76925081114a
devatherock <devatherock@gmail.com>
2023-12-22 09:59:22 -0600

    test: Used @NonCPS again

--- jobs/pipelines/trigger_another.groovy
+++ jobs/pipelines/trigger_another.groovy
@@ -1,3 +1,14 @@
+@NonCPS
+void triggerStashedFileJob(File testFile) {
+    def factory = new org.apache.commons.fileupload.disk.DiskFileItemFactory()
+    def fileItem = factory.createItem('file', 'text/plain', false, 'test-copy.txt')
+    fileItem.outputStream << testFile.bytes
+
+    build job: 'stashed-file', parameters: [
+        new io.jenkins.plugins.file_parameters.StashedFileParameterValue('INPUT_FILE', fileItem),
+    ]
+}
+
 pipeline {
     agent any
 
@@ -11,16 +22,7 @@ pipeline {
 
                 script {
                     File testFile = new File("${env.WORKSPACE}/test.txt")
-                    // def factory = new org.apache.commons.fileupload.disk.DiskFileItemFactory()
-                    // def fileItem = factory.createItem('file', 'text/plain', false, 'test-copy.txt')
-                    // fileItem.outputStream << testFile.bytes
-
-                    // build job: 'stashed-file', parameters: [
-                    //     new io.jenkins.plugins.file_parameters.StashedFileParameterValue('INPUT_FILE', fileItem),
-                    // ]
-                    build job: 'stashed-file', parameters: [
-                        base64File(name: 'INPUT_FILE', base64: Base64.encoder.encodeToString(testFile.bytes)),
-                    ]
+                    triggerStashedFileJob(testFile)
                 }
             }
         }
