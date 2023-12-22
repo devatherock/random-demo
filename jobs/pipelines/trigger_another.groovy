7226e74c39ab912b83c0f046ec526292bbc42436
devatherock <devatherock@gmail.com>
2023-12-22 09:49:05 -0600

    test: Used base64 file again

--- jobs/pipelines/trigger_another.groovy
+++ jobs/pipelines/trigger_another.groovy
@@ -11,12 +11,15 @@ pipeline {
 
                 script {
                     File testFile = new File("${env.WORKSPACE}/test.txt")
-                    def factory = new org.apache.commons.fileupload.disk.DiskFileItemFactory()
-                    def fileItem = factory.createItem('file', 'text/plain', false, 'test-copy.txt')
-                    fileItem.outputStream << testFile.bytes
+                    // def factory = new org.apache.commons.fileupload.disk.DiskFileItemFactory()
+                    // def fileItem = factory.createItem('file', 'text/plain', false, 'test-copy.txt')
+                    // fileItem.outputStream << testFile.bytes
 
+                    // build job: 'stashed-file', parameters: [
+                    //     new io.jenkins.plugins.file_parameters.StashedFileParameterValue('INPUT_FILE', fileItem),
+                    // ]
                     build job: 'stashed-file', parameters: [
-                        new io.jenkins.plugins.file_parameters.StashedFileParameterValue('INPUT_FILE', fileItem),
+                        base64File(name: 'INPUT_FILE', base64: Base64.encoder.encodeToString(testFile.bytes)),
                     ]
                 }
             }
