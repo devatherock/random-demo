d4b90fbddb433c73eb9fbffbbe05c3073a9348bd
devatherock <devatherock@gmail.com>
2023-12-22 07:49:03 -0600

    SO-77644443: Found file name from environment variable

--- jobs/pipelines/stashed_file.groovy
+++ jobs/pipelines/stashed_file.groovy
@@ -2,26 +2,22 @@ pipeline {
     agent any
 
     parameters {
-        stashedFile(name: 'inputFile', description: 'some file')
+        // Requires https://plugins.jenkins.io/file-parameters/ plugin
+        stashedFile(name: 'INPUT_FILE', description: 'some file')
     }
 
     stages {
         stage('Find file name') {
             steps {
-               deleteDir() // To cleanup workspace 
-               sh 'mkdir test'
+                deleteDir() // To cleanup workspace 
+                unstash 'INPUT_FILE'
+                sh 'mv INPUT_FILE $INPUT_FILE_FILENAME'
 
-               dir('test') {
-                   unstash 'inputFile'
-               }
-
-               script {
-                   String fileName = sh(script: '''#!/bin/bash
-                       cd test
-                       ls
-                   ''', returnStdout: true).trim()
-                   println("The input filename: ${fileName}")
-               }
+                script {
+                    String fileName = env.INPUT_FILE_FILENAME
+                    String fileContents = sh(script: "cat ${fileName}", returnStdout: true).trim()
+                    println("File name: ${fileName}, contents: ${fileContents}")
+                }
             }
         }
     }
